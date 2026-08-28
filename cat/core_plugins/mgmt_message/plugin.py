5f5d3c325c4350294b204e022b139ea003b0872f
Andrea Sterbini <andrea.sterbini@uniroma1.it>
2026-08-28 14:51:09 +0200

    management plugin fix

--- cat/core_plugins/mgmt_message/plugin.py
+++ cat/core_plugins/mgmt_message/plugin.py
@@ -2,12 +2,12 @@ from cat import hook
 from cat.auth.permissions import AuthResource
 from cat.db.cruds import settings as crud_settings
 from cat.db.database import DEFAULT_SYSTEM_KEY
-
+from cat import log
 
 @hook(priority=1)
 async def auth_request(local_user, agent_id, connection, **kwargs):
     # read global settings
-    setting = await crud_settings.get_setting_by_name(DEFAULT_SYSTEM_KEY, "mgmt-message")
+    setting = await crud_settings.get_setting_by_name(DEFAULT_SYSTEM_KEY, "mgmt_message")
     value = (setting or {}).get("value") or {}
     if not value.get("management_active", False):
         return None  # allow
@@ -17,4 +17,5 @@ async def auth_request(local_user, agent_id, connection, **kwargs):
     if str(AuthResource.SYSTEM) in permissions:
         return None  # allow
 
+    log.info(f"MANAGEMENT MODE: Access negated to user {local_user}.")
     return value.get("management_message", "Access denied")
